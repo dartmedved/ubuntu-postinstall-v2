f134ebe6bc18dfec479e1cebe7313337514f593b
Mike Pustovalov <pustovalov.mike@gmail.com>
2018-03-04 22:55:59 +0300

    remove opera scripts

--- repositories/install-opera-repo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
