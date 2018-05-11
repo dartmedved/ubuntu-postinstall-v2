c5265bf96b275dbe9c94eb38f4f4c2b28f2a6cd2
Mike Pustovalov <dartbear@gmail.com>
2018-05-11 21:33:46 +0300

    u 18.04 mods

--- to-migration/ubuntu-repositories.sh
+++ to-migration/ubuntu-repositories.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 add-apt-repository ppa:webupd8team/atom
-add-apt-repository ppa:webupd8team/terminix
+#add-apt-repository ppa:webupd8team/terminix
 
 wget -O - https://tagplus5.github.io/vscode-ppa/ubuntu/gpg.key | sudo apt-key add -
 wget -O /etc/apt/sources.list.d/vscode.list https://tagplus5.github.io/vscode-ppa/ubuntu/vscode.list
