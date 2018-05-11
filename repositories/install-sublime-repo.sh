6e4afef066155248dbbb1e2367ccc6fc8f37d6e1
Mike Pustovalov <dartbear@gmail.com>
2018-05-11 22:41:25 +0300

    18.04 updates

--- repositories/install-sublime-repo.sh
+++ repositories/install-sublime-repo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update 
