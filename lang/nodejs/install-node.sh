d972e69f1e4ab6bb0394a561e3c41a26221a82bb
Mike Pustovalov <dartbear@gmail.com>
2018-11-05 17:23:30 +0300

    update nodejs install  script

--- lang/nodejs/install-node.sh
+++ lang/nodejs/install-node.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
