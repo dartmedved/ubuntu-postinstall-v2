79ec3854b21519388423f6f0d61c187481ab0d73
Mike Pustovalov <dartbear@gmail.com>
2018-11-30 23:36:14 +0300

    install gitkraken

--- devtools/install-gitkraken.sh
+++ devtools/install-gitkraken.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-export KRAKEN_URL=https://release.gitkraken.com/linux/gitkraken-amd64.deb
-export KRAKEN_FILE=/tmp/gitkraken-amd64.deb
-
-echo $KRAKEN_URL "--->" $KRAKEN_FILE
-rm -f $KRAKEN_FILE
-http --download --pretty all --style solarized  --verbose --output $KRAKEN_FILE $KRAKEN_URL
-sudo dpkg -i $KRAKEN_FILE
+sudo snap install gitkraken
