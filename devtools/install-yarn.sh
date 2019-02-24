15d42d9767fcc9c1e67035e26aea588342c61c50
Mike Pustovalov <dartbear@gmail.com>
2019-02-24 23:46:45 +0300

    -bower +yarn

---
+++ devtools/install-yarn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt -y remove cmdtest
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt -y install yarn
--- lang/nodejs/postinstall-node.sh
+++ lang/nodejs/postinstall-node.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo npm install -g yo
 sudo npm install -g grunt-cli 
-sudo npm install -g bower 
 sudo npm install -g gulp 
 sudo npm install -g generator-jhipster
 sudo npm install -g typescript
