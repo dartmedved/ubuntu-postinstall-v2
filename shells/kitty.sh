af698b22024c981b836c9114b999158dd1849022
Mike Pustovalov <dartbear@gmail.com>
2019-04-18 21:01:42 +0300

    add kitty deps

--- shells/kitty.sh
+++ shells/kitty.sh
@@ -6,6 +6,7 @@
 # https://github.com/kovidgoyal/kitty
 
 sudo apt install python3-dev git gcc build-essential pkg-config libharfbuzz-dev libpng-dev libfontconfig1-dev libgl1-mesa-dev libxkbcommon-x11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
+sudo apt install git virtualenv build-essential python3-dev libdbus-glib-1-dev libgirepository1.0-dev
 #cd /opt
 #sudo git clone https://github.com/kovidgoyal/kitty
 #cd /opt/kitty
