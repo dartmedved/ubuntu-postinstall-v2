ae1b51944fbc6d1cb8bd2d9209f7e1f52ef8b361
Mike Pustovalov <pustovalov.mike@yandex.ru>
2022-03-11 22:33:14 +0300

    update sdkman

--- sdkman/install-sdk.sh
+++ sdkman/install-sdk.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+
 sdk install java 
 sdk install maven 
 sdk install groovy 
 sdk install gradle 
 sdk install kotlin
 sdk install visualvm
+sdk install sbt
+sdk install leiningen
+
 sdk upgrade
