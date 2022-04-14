fae9a58f656c0d6009c2369fc294d5a0cf271c52
Mike Pustovalov <pustovalov.mike@yandex.ru>
2022-04-14 19:54:36 +0300

    update full update script

--- full-update.sh
+++ full-update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo ----[project update]------------------------------------------------------
 git pull
+echo ----[linux update]--------------------------------------------------------
+sudo ./update.sh
 echo ----[sdk update]----------------------------------------------------------
 source ~/.sdkman/bin/sdkman-init.sh 
 sdk ug
-echo ----[linux update]--------------------------------------------------------
-sudo ./update.sh
 echo ----[rust update]---------------------------------------------------------
 rustup update
 echo ----[npm update]----------------------------------------------------------
+sudo npm install -g npm
 sudo npm -g update 
 # echo ----[flutter update]------------------------------------------------------
 # flutter upgrade 
