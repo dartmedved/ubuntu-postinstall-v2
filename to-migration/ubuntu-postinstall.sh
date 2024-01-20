0425dfe8196eeaec33c87569b4ff3795c7409ba8
Mike Pustovalov <pustovalov.mike@yandex.ru>
2024-01-21 01:07:23 +0300

    fixes & updates

--- to-migration/ubuntu-postinstall.sh
+++ to-migration/ubuntu-postinstall.sh
@@ -4,29 +4,33 @@ apt -y install curl wget w3m
 apt -y install p7zip-full mc htop iotop
 apt -y install git mercurial subversion
 apt -y install ubuntu-restricted-extras
-apt -y install python3
+apt -y install python3 python3-all python3-all-dev
 apt -y install unzip unrar 
 apt -y install ruby ruby-dev
 apt -y install openssh-server
-apt -y install openjdk-8-jdk
-apt -y install ffmpeg gstreamer-tools
+apt -y install openjdk-11-jdk
+apt -y install ffmpeg
+apt -y install gstreamer1.0-tools
+apt -y install yt-dlp
 apt -y install mplayer2
-apt -y install libzen0v5
-apt -y install libmediainfo0v5
+apt -y install mpv
+#apt -y install libzen0v5
+#apt -y install libmediainfo0v5
 apt -y install vlc
 apt -y install golang-go
 apt -y install dstat
 apt -y install glances
 apt -y install vim-nox
+apt -y install neovim
 apt -y install bmon slurm tcptrack iftop nethogs
-apt -y install taskwarrior
+#apt -y install taskwarrior
 apt -y install galculator
 apt -y install pwgen
 apt -y install w3m
 apt -y install vlock
 
 #go tools
-apt -y install gocode
+apt -y install gopls
 apt -y install golang-golang-x-tools
 
 #terminal emulators
