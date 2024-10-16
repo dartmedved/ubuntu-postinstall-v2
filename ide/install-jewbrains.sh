74637ed1183da57cf7c0fcbba291e38e870569f5
Mike Pustovalov <pustovalov.mike@yandex.ru>
2024-10-17 00:17:18 +0300

    +ide section

---
+++ ide/install-jewbrains.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo snap refresh 
+sudo snap intellij-idea-community --classic
+sudo snap pycharm-community --classic
--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 sudo apt update 
 sudo apt upgrade -y
 sudo apt autoclean && sudo apt autoremove
+sudo snap refresh
