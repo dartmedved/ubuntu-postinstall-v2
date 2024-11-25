0818fd54198ebbe0ed611d1baad3e8933318729d
Mike Pustovalov <pustovalov.mike@yandex.ru>
2024-11-25 17:50:48 +0300

    🐛 (Clean scripts): Fix apt bug

--- cleanup/clean-thunderbird.sh
+++ cleanup/clean-thunderbird.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt -y remove thunderbird
-sudo apt -y remove thunderbird*
+sudo apt remove -y thunderbird
+sudo apt remove -y thunderbird*
 sudo apt -y autoremove
 sudo snap remove thunderbird
 
