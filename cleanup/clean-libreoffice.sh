d455a6d70a03b98c5e7e23b894748638bd398e12
Mike Pustovalov <pustovalov.mike@yandex.ru>
2024-11-25 17:48:42 +0300

    🗑️ Add clean script for libreoffice

---
+++ cleanup/clean-libreoffice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt remove -y libreoffice*
+sudo apt autoremove -y 
+
--- cleanup/clean-thunderbird.sh
+++ cleanup/clean-thunderbird.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt -y remove thunderbird
+sudo apt -y remove thunderbird*
 sudo apt -y autoremove
 sudo snap remove thunderbird
 
