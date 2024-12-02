e0434b39628d230234bb0b7e25f38aece3683e88
Mike Pustovalov <pustovalov.mike@yandex.ru>
2024-12-02 22:39:54 +0300

    🚧 (bootstrap): + nala and build essentials

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt install -y mc htop btop curl wget git mercurial subversion p7zip-full 
+sudo apt install -y mc htop btop curl wget git mercurial subversion p7zip-full build-essential nala 
 
  
